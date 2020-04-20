fd86a1247c6fee42879d36f04e2dfb472d3be1db
Nimrod Burg <burgnim@post.bgu.ac.il>
2020-04-20 16:02:21 +0300

    progress in DAL + new Task.cs in Service Layer

---
+++ Backend/DataAccessLayer/Objects/DALObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer.Objects
+{
+    abstract class DALObject<T> where T : DALObject<T>
+    {
+        // takes this instance of a DAL object and returns it as a json string
+        public string ToJson()
+        {
+            return (JsonSerializer.Serialize(this, this.GetType()));
+        }
+
+        public static T FromJson(string json)
+        {
+            // takes an already read json string from dal controller and returns it as an object
+
+            return (JsonSerializer.Deserialize<T>(json));
+           
+        }
+
+
+    }
+}
---
+++ Backend/DataAccessLayer/Objects/GuyKabalaSaveToJson.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer.Objects
+{
+    class GuyKabalaSaveToJson
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public GuyKabalaSaveToJson(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void tojson()
+        {
+            string objectAsJason = JsonSerializer.Serialize(this, this.GetType());
+            File.WriteAllText("testfile.json", objectAsJason);
+        
+        }
+
+        public void readingFromJson()
+        {
+            GuyKabalaSaveToJson a = new GuyKabalaSaveToJson("justID", "justNAME");
+            a.tojson(); // writes to the json file
+
+            if (File.Exists("testfile.json"))
+            {
+                string objectAsJson = File.ReadAllText("testfile.json");
+                a = JsonSerializer.Deserialize<GuyKabalaSaveToJson>(objectAsJson);
+            }
+            else
+            {
+                //create empty userController
+            }
+            
+
+        }
+
+    }
+}

fc84790df1a8ce4d015dfd4ef161f05f8f65ab2e
Nimrod Burg <burgnim@post.bgu.ac.il>
2020-04-21 20:37:57 +0300

    json read-write, dal user, console app for tests

--- Backend/DataAccessLayer/Objects/User.cs
+++ Backend/DataAccessLayer/Objects/User.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IntroSE.Kanban.Backend.DataAccessLayer.Objects
 {
-    class User
+    public class User : DALObject<User>
     {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string nickname { get; set; }
+
+        public User(string email, string password, string nickname)
+        {
+            this.email = email;
+            this.password = password;
+            this.nickname = nickname;
+        }
+
+        public User()
+        {
+
+        }
+
+        public DALObject<User> Import(string email)
+        {
+            return DALObject<User>.FromJson(DALController.Read(this.GetSafeFilename(email) + ".json"));
+        }
+        public override void Save()
+        {
+            DALController.Write(this.GetSafeFilename(email) + ".json", this.ToJson());
+           
+        }
+
     }
 }
---
+++ TestingConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace TestingConsoleApp
+{
+    class Program
+    {
+        public static string GetSafeFilename(string filename)
+        {
+            char[] remove = {',', '.', '@', '/', ':'};
+            filename=string.Join("", filename.Split(remove));
+            return (string.Join("", filename.Split(Path.GetInvalidFileNameChars())));
+        }
+
+        public static void Main(string[] args)
+        {
+            var user = new IntroSE.Kanban.Backend.DataAccessLayer.Objects.User("ido12@banna.co.il", "passy", 
+                "nickname");
+            Console.WriteLine(user.ToJson());
+            Console.ReadKey();
+            user.Save();
+
+            Console.WriteLine(user.Import(user.email).ToString());
+            Console.ReadKey();
+        
+        }
+    }
+}
